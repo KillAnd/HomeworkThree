aa25ffeb4d0ce1e6a1da2f7448b2dd667b720978
kila1 <kila14081996@gmail.com>
2024-02-16 10:35:50 +0300

    1 задание

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        byte oneTic = 8;
+        short twoTic = 100;
+        int threeTic = 200;
+        long forTic = 1000;
+        float fiveTic = 1.3f;
+        double sixTic = 13.88;
+
+        System.out.println("Значение переменной oneTic с типом byte равно " + oneTic);
+        System.out.println("Значение переменной twoTic с типом short равно " + twoTic);
+        System.out.println("Значение переменной threeTic с типом int равно " + threeTic);
+        System.out.println("Значение переменной forTic с типом long равно " + forTic);
+        System.out.println("Значение переменной fiveTic с типом float равно " + fiveTic);
+        System.out.println("Значение переменной sixTic с типом double равно " + sixTic);
+    }
+}
