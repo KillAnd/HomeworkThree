f437743b11f791d1cb589387f5796a282447c446
kila1 <kila14081996@gmail.com>
2024-02-17 02:43:55 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        short fullList = 480;
-        byte p = 23;
-        byte s = 27;
-        byte a = 30;
-        int together = p + s + a;
-        int listOneStudent = fullList/together;
+//если за 2 минуты производится 16 бутылок, следовательно за 1 минуту производится 8 бутылок.
+        byte batles = 8;
+        byte time1 = 20; //минуты
+        short time2 = 1440; //сутки
+        int time3 = 1440*30; //месяц
 
-        System.out.println("На одного ученика выделено " + listOneStudent + " листов");
+        System.out.println("За 20 минут машина произвела " + batles*time1 + " штук бутылок");
+        System.out.println("За сутки машина произвела " + batles*time2 + " штук бутылок");
+        System.out.println("За 3 суток машина произвела " + batles*(time2*3) + " штук бутылок");
+        System.out.println("За 1 месяц машина произвела " + batles*time3 + " штук бутылок");
     }
 }
