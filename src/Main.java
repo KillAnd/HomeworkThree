1e082bec883506c790aa1ac43567ec6e2b3f3333
kila1 <kila14081996@gmail.com>
2024-02-16 11:11:17 +0300

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        short fullList = 480;
+        byte p = 23;
+        byte s = 27;
+        byte a = 30;
+        int together = p + s + a;
+        int listOneStudent = fullList/together;
 
-        byte oneTic = 67;
-        short twoTic = 569;
-        short c = -159;
-        int threeTic = 27897;
-        long forTic = 987678965549L;
-        float fiveTic = 27.12f;
-        double sixTic = 2.786;
-
-        System.out.println("Значению " + oneTic + " присваиваю тип byte");
-        System.out.println("Значению " + twoTic + " присваиваю тип short");
-        System.out.println("Значению " + c + " присваиваю тип short");
-        System.out.println("Значению " + threeTic + " присваиваю тип int");
-        System.out.println("Значению " + forTic + " присваиваю тип long");
-        System.out.println("Значению " + fiveTic + " присваиваю тип float");
-        System.out.println("Значению " + sixTic + " присваиваю тип double");
+        System.out.println("На одного ученика выделено " + listOneStudent + " листов");
     }
 }
