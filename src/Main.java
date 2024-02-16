98011348c5d7d2bebe5cf4308bb64172e53b8405
kila1 <kila14081996@gmail.com>
2024-02-17 02:58:07 +0300

    5 задание

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//если за 2 минуты производится 16 бутылок, следовательно за 1 минуту производится 8 бутылок.
-        byte batles = 8;
-        byte time1 = 20; //минуты
-        short time2 = 1440; //сутки
-        int time3 = 1440*30; //месяц
 
-        System.out.println("За 20 минут машина произвела " + batles*time1 + " штук бутылок");
-        System.out.println("За сутки машина произвела " + batles*time2 + " штук бутылок");
-        System.out.println("За 3 суток машина произвела " + batles*(time2*3) + " штук бутылок");
-        System.out.println("За 1 месяц машина произвела " + batles*time3 + " штук бутылок");
+        byte whiteColor = 2;
+        byte brownColor = 4;
+        byte fullColor = 120;
+        int totalClassRoom = fullColor/(whiteColor+brownColor);
+        int totalWhiteColor = totalClassRoom*whiteColor;
+        int totalBrownColor = totalClassRoom*brownColor;
+
+        System.out.println("В школе, где " + totalClassRoom + " классов, нужно " + totalWhiteColor + " банок белой краски и " + totalBrownColor + " банок коричневой краски");
     }
 }
