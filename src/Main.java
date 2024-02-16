617b9521861c67dc3e24474aa3bfba9d55056f2c
kila1 <kila14081996@gmail.com>
2024-02-16 10:55:43 +0300

    2 задание присваивание типов

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        byte oneTic = 8;
-        short twoTic = 100;
-        int threeTic = 200;
-        long forTic = 1000;
-        float fiveTic = 1.3f;
-        double sixTic = 13.88;
+        byte oneTic = 67;
+        short twoTic = 569;
+        short c = -159;
+        int threeTic = 27897;
+        long forTic = 987678965549L;
+        float fiveTic = 27.12f;
+        double sixTic = 2.786;
 
-        System.out.println("Значение переменной oneTic с типом byte равно " + oneTic);
-        System.out.println("Значение переменной twoTic с типом short равно " + twoTic);
-        System.out.println("Значение переменной threeTic с типом int равно " + threeTic);
-        System.out.println("Значение переменной forTic с типом long равно " + forTic);
-        System.out.println("Значение переменной fiveTic с типом float равно " + fiveTic);
-        System.out.println("Значение переменной sixTic с типом double равно " + sixTic);
+        System.out.println("Значению " + oneTic + " присваиваю тип byte");
+        System.out.println("Значению " + twoTic + " присваиваю тип short");
+        System.out.println("Значению " + c + " присваиваю тип short");
+        System.out.println("Значению " + threeTic + " присваиваю тип int");
+        System.out.println("Значению " + forTic + " присваиваю тип long");
+        System.out.println("Значению " + fiveTic + " присваиваю тип float");
+        System.out.println("Значению " + sixTic + " присваиваю тип double");
     }
 }
